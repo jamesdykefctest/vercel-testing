186f7553e870ccdb7a3deb24aa272551c47f47e8
James Dyke <jcdyke@gmail.com>
2022-02-14 09:58:35 -0800

    Update tests

--- tests/tests/example.spec.ts
+++ tests/tests/example.spec.ts
@@ -1,7 +1,11 @@
 import { test, expect } from '@playwright/test';
 
-test('basic test', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  const title = page.locator('.navbar__inner .navbar__title');
-  await expect(title).toHaveText('Playwright');
+test.describe("Browse bounties", () => {
+  test.beforeEach(async ({ page, baseURL }) => {
+    await page.goto(baseURL);
+  });
+
+  test("should show a list of drops", async ({ page }) => {
+    await expect(page.locator("data-test-id=dyke")).toBeTruthy();
+  });
 });
