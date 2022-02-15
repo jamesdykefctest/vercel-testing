7556a79b94872ae3c3c0840a882d3e4a2ff7de73
James Dyke <jcdyke@gmail.com>
2022-02-15 10:41:08 -0800

    pass

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App" data-test-id="dyke">
       <header className="App-header">
         <p className="title">
-          Just testing example
+          Just testing
         </p>
         <a
           className="App-link"
